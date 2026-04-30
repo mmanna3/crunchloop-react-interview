f03fa311d113fa0d24920926897d939b9dc301d9
mmanna3 <mmmannna3@gmail.com>
2026-04-30 18:57:05 -0300

    chore: install and configure react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { createQueryClient } from './lib/react-query'
+
+const queryClient = createQueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
